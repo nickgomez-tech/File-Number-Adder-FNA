2ef5ae4c44c30ca7ad4392366a3a15a45fc047c4
Nick Gomez <71398770+nickgomez-tech@users.noreply.github.com>
2022-01-08 18:32:42 -0500

    Add files via upload

---
+++ File.h
@@ -0,0 +1,10 @@
+#ifndef FILE_H
+#define FILE_H
+
+int file(int a, int b) {
+	std::cout << "Welcome to File Number Adder Library executing\n";
+	std::cout << "The answer is" << a + b;
+	return 0;
+}
+
+#endif
